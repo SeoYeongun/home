49b20a50486034439342eeb9c36d6401485693ea
SeoYeongun <voe697754@gmail.com>
2024-01-11 19:35:57 +0900

    home work

---
+++ test/src/sec01/exam01/From1To10.java
@@ -0,0 +1,13 @@
+package sec01.exam01;
+
+public class From1To10 {
+
+	public static void main(String[] args) {
+		int total = 0;
+		for(int i=1; i<=10; i++) {
+			total += i;
+		}
+		System.out.println("1~10의 합: " + total);
+	}
+
+}
